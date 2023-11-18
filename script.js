29bd2e5e69e58b2ab8fe45a01e596122dcc014a0
Aditi Lal <139861603+lal15aditi@users.noreply.github.com>
2023-11-18 15:30:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function onDOMLoaded() {
+    const body = document.body;
+    body.innerText = "DOM load success";
+  }
+
+  // Event listener for DOMContentLoaded
+  document.addEventListener("DOMContentLoaded", onDOMLoaded);
